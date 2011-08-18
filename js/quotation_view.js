077fb843edacaac90b20d36ded704d9798c34f5c
Daniel Lin <pct@pctwing.twbbs.org>
2011-08-19 00:20:40 +0800

    new: missing files

---
+++ js/quotation_view.js
@@ -0,0 +1,20 @@
+$(function() {
+    $.count_prices = function() {
+        var sub_total_price = 0;
+        var vat = 0.05;
+        var vat_price = 0;
+        var total_price = 0;
+        
+        $('.item_price').each(function(i, el) {
+            $.item_quantity = $(el).parent().find('.item_quantity');
+            $.item_sub_total = $(el).parent().find('.item_sub_total');
+            var item_quantity = (isNaN(parseInt($.item_quantity.html()))) ? 0 : parseInt($.item_quantity.html());
+            var item_price = (isNaN(parseInt($(el).html()))) ? 0 : parseInt($(el).html());
+            var tmp_value = item_quantity * item_price;
+            $.item_sub_total.html(tmp_value);
+        });
+    }
+
+    // 啟動時先算第一次
+    $.count_prices();
+});
---
+++ lib/Haanga/lib/Haanga/Extension/Filter/Nl2br.php
@@ -0,0 +1,7 @@
+<?php
+
+class Haanga_Extension_Filter_Nl2br
+{
+    public $php_alias = "nl2br";
+    public $is_safe = TRUE;
+}
