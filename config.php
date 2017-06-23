8696b25fd73b5f3fc573cc19fab754dc93528d60
Jin-Sih Lin <linpct@gmail.com>
2017-06-23 17:37:21 +0800

    add date_default_timezone_set for newer php version

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+$TIME_ZONE = 'Asia/Taipei';
+
 $DB_HOST = 'localhost';
 $DB_NAME = '4money';
 $DB_USER = 'root';
@@ -8,4 +10,5 @@ $DB_PASS = '';
 $COOKIES_SECRET_KEY = 'PLEASE CHANGE THIS';
 $WKHTMLTOPDF_BIN_PATH = '/usr/local/bin/wkhtmltopdf'; # if you need pdf support
 
+date_default_timezone_set($TIME_ZONE);
 ?>
